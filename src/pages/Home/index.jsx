ba9414c2c30baf6439bb710b630a09e3ed6aee23
anthonymackiw@outlook.fr <94121080+Popo30240@users.noreply.github.com>
2025-06-02 15:17:11 +0200

    V 0.12 Correction: erreur dans la console dû à un fichier inexistant qui était appelé dans l'index.html.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
 // Components
 import Card from '../../components/Card';
 import Banner from '../../components/Banner';
 
 function Home() {
-  const [Data, setData] = useState([]);
+
+  // Hook pour la navigation
+  // Utilisé pour rediriger vers une autre page
   const navigate = useNavigate();
 
+  // Initialisation un tableau vide pour stocker les données
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    fetch('/Data/Data.json')
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/Data/Data.json');
         if (!response.ok) throw new Error('Fichier JSON non trouvé');
-        return response.json();
-      })
-      .then((json) => {
+        const json = await response.json();
         if (!json || json.length === 0) {
           navigate('/404');
         } else {
+          // Si les données sont valides, on les stocke dans l'état
+          // setData est une fonction qui met à jour l'état data
           setData(json);
         }
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error('Erreur chargement :', error);
         navigate('/404');
-      });
-  }, [navigate]);
+      }
+    };
+    // useEffect est un hook qui permet d'exécuter du code après le rendu du composant
+    // Appel de la fonction fetchData pour récupérer les donnée
+    fetchData();
+  }, [navigate]); // Si on est redirigé vers une autre page,
+  //  on re-exécute le code jusqu'à ce que les données valides soient chargées .
 
   return (
     <div>
       <Banner />
       <div className="container-home">
-        {Data.map((logement) => (
+        {data.map((logement) => (
           <Card key={logement.id} logement={logement} />
         ))} 
       </div>
